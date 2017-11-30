094f69ecf648f9264057f22cb346399721cfae97
Luke Robinson <robinsonlm@live.co.uk>
2017-11-30 16:30:13 +0100

    Navigation and simple player movement implemented. Brazier lights now flicker

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator), typeof(NavMeshAgent))]
+public class PlayerAnimationController : MonoBehaviour
+{
+	private Animator animator;
+	private NavMeshAgent navMeshAgent;
+    // Use this for initialization
+    void Start()
+    {
+		animator = GetComponent<Animator>();
+		navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		animator.SetFloat("PlayerVelocity", navMeshAgent.velocity.magnitude);
+    }
+}
